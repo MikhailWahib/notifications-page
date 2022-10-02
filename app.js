7e16f57e60ec57ce47d137af336b5ca576fb71e6
MikhailWahib <112858669+MikhailWahib@users.noreply.github.com>
2022-10-02 07:12:53 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const notificationsArr = document.querySelectorAll('.notification-card')
+const circlesArr = document.querySelectorAll('.circle')
+const markAllBtn = document.querySelector('#mark-all')
+let notificationsCount = document.querySelector('#notifications-count').innerText
+console.log(notificationsCount);
+
+for(let i = 0 ; i < notificationsArr.length; i++) {
+    let isActive = true
+    notificationsArr[i].addEventListener('click', () => {
+        if(isActive) {
+            notificationsArr[i].classList.remove('new-notification')
+            circlesArr[i].classList.remove('circle')
+            notificationsCount -= 1
+            document.querySelector('#notifications-count').innerText = notificationsCount
+            isActive = false;
+            console.log(notificationsCount);
+        }
+    })
+    markAllBtn.addEventListener('click', () => {
+        if(isActive) {
+            notificationsArr[i].classList.remove('new-notification')
+            circlesArr[i].classList.remove('circle')
+            notificationsCount = 0
+            document.querySelector('#notifications-count').innerText = notificationsCount
+            isActive = false
+        }
+    })
+}
+
